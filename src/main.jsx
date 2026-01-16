93e61fe828cf1698d400a44a7f77a64f7860d4b5
emamontenegro <montenegroemanuel995@gmail.com>
2026-01-15 22:02:10 -0300

    fix: case-sensitive import for language context

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from 'react-router'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { LanguageProvider } from './context/languageContext.jsx';
+import { LanguageProvider } from './context/LanguageContext.jsx';
 import "./styles/reset.css";
 import "./styles/variables.css";
 import "./styles/global.css";
